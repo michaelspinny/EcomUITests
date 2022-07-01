3a6e1fdb71868b1ba409f93d9e2c6df93a00313f
Mykhailo_Portianov <mykhailo.portianov@boemm.eu>
2022-07-01 16:46:24 +0300

    EComTests (part 1)

---
+++ src/main/java/org/example/Login.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Login {
+
+    private static final String baseURl = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
+    private static final By emailAddressFieldLocator = By.xpath("//*[@id='email']");
+    private static final By passwordFieldLocator = By.xpath("//*[@id='passwd']");
+    private static final By signInButtonLocator = By.xpath("");
+
+    public static void main(String[] args) {
+        loginTest();
+
+    }
+
+    public static void loginTest() {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        driver.get(baseURl);
+        driver.manage().window().maximize();
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(emailAddressFieldLocator)));
+        driver.findElement(emailAddressFieldLocator).sendKeys("test@mail.com");
+
+
+    }
+}
