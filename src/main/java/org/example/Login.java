c0d772bab828d9e270d911879bdef3eea58c5281
Mykhailo_Portianov <mykhailo.portianov@boemm.eu>
2022-07-01 17:10:42 +0300

    EComTests (part 2)

--- src/main/java/org/example/Login.java
+++ src/main/java/org/example/Login.java
@@ -12,9 +12,11 @@ import java.time.Duration;
 public class Login {
 
     private static final String baseURl = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
+    private static final String loggedInURL = "http://automationpractice.com/index.php?controller=my-account";
     private static final By emailAddressFieldLocator = By.xpath("//*[@id='email']");
     private static final By passwordFieldLocator = By.xpath("//*[@id='passwd']");
-    private static final By signInButtonLocator = By.xpath("");
+    private static final By signInButtonLocator = By.xpath("//*[@id='SubmitLogin']/span");
+
 
     public static void main(String[] args) {
         loginTest();
@@ -31,8 +33,20 @@ public class Login {
         driver.manage().window().maximize();
 
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(emailAddressFieldLocator)));
-        driver.findElement(emailAddressFieldLocator).sendKeys("test@mail.com");
+        driver.findElement(emailAddressFieldLocator).sendKeys("lamontae.julius@moondoo.org");
 
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(passwordFieldLocator)));
+        driver.findElement(passwordFieldLocator).sendKeys("Aa111111/");
 
+        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(signInButtonLocator)));
+        driver.findElement(signInButtonLocator).click();
+
+        String url = driver.getCurrentUrl();
+        if (url.equals(loggedInURL)) {
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+        }
+        driver.quit();
     }
 }
