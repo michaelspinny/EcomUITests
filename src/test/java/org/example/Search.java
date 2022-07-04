6889bf7b15738dd80fef71079197b6f9b8dc3063
Mykhailo_Portianov <mykhailo.portianov@boemm.eu>
2022-07-04 21:41:03 +0300

    EComTestsVer2 (part 3)

--- src/test/java/org/example/Search.java
+++ src/test/java/org/example/Search.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
@@ -20,16 +21,15 @@ public class Search {
     private static final By signInButtonLocator = By.xpath("//*[@id='SubmitLogin']/span");
     private static final By searchBarLocator = By.xpath("//*[@id='search_query_top']");
     private static final By searchBarSubmit = By.xpath("//*[@id='searchbox']/button");
+    private static final By searchSuccessTextLocator = By.xpath("//h1[@class='page-heading  product-listing']//span[@class='heading-counter']");
 
     private static final String userLoginEmail = "lamontae.julius@moondoo.org";
     private static final String userLoginPassword = "Aa111111/";
     private static final String productSearchQuery = "dress";
+    private static final String searchSuccessTextSample = "results have been found";
 
 
-    public static void main(String[] args) {
-        searchTest();
-    }
-
+    @Test
     public static void searchTest() {
 
         WebDriverManager.chromedriver().setup();
@@ -54,7 +54,10 @@ public class Search {
         wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(searchBarSubmit)));
         driver.findElement(searchBarSubmit).click();
 
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(searchSuccessTextLocator)));
+        String searchSuccessTextValue = driver.findElement(searchSuccessTextLocator).getText();
 
+        Assert.assertTrue(searchSuccessTextValue.contains(searchSuccessTextSample));
 
         driver.quit();
 
